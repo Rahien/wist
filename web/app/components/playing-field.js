19f008876af0be3fe442464ace4ebabf5885af5c
Karel Kremer <karel.kremer@gmail.com>
2014-10-12 19:00:36 +0200

    added style when card can be dropped

--- web/app/components/playing-field.js
+++ web/app/components/playing-field.js
@@ -11,6 +11,15 @@ export default Ember.Component.extend({
 	    scope: "cards",
 	    drop: function( event, ui ) {
 		self.sendAction("playCard",  ui.draggable[0].dragged);
+		ui.draggable.removeClass("over");
+	    },
+	    over: function( event, ui) {
+		if(!ui.draggable.hasClass("over")){
+		    ui.draggable.addClass("over");
+		}
+	    },
+	    out: function( event, ui){
+		ui.draggable.removeClass("over");
 	    }
 	});
     }
